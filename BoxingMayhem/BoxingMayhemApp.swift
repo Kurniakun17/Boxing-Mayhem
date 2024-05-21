182d4fe1735b98789316a989ae7bebc274cf4785
Kurniakun17 <kurniakharisma17@gmail.com>
2024-05-21 09:14:05 +0700

    Initial Commit

---
+++ BoxingMayhem/BoxingMayhemApp.swift
@@ -0,0 +1,17 @@
+//
+//  BoxingMayhemApp.swift
+//  BoxingMayhem
+//
+//  Created by Kurnia Kharisma Agung Samiadjie on 21/05/24.
+//
+
+import SwiftUI
+
+@main
+struct BoxingMayhemApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BoxingMayhem/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  BoxingMayhem
+//
+//  Created by Kurnia Kharisma Agung Samiadjie on 21/05/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
